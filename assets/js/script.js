fd7ce59b083287c287e48d07b93148e92b39df66
ChristianUcheCOBC <733278@student.cityofbristol.ac.uk>
2024-12-02 09:55:41 +0000

    change js to see styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,4 @@
 document.addEventListener("DOMContentLoaded", function (){
 
-    const checkbox = getElementsById(nav-tog);
-    const header_div = getElementsByClassName(header_div);
-
-    //add event-listener to the div!
-    header_div.addEventListener("click", function(){
-        if(checkbox.checked){
-            header_div.style.position = "";
-        }
-        else{
-            // write the original style for the header-div out.
-        }
-    });
+    //add code here!!!
 });
