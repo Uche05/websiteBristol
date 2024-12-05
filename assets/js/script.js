eb313642424490e49fd816d1b0a1286545ad37e6
Uche05 <uchekpadeuwa@gmail.com>
2024-12-05 11:14:28 +0000

    Add grid images and like effect

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,7 @@
-document.addEventListener("DOMContentLoaded", function (){
-
-    const checkbox = getElementsById(nav-tog);
-    const header_div = getElementsByClassName(header_div);
-
-    //add event-listener to the div!
-    header_div.addEventListener("click", function(){
-        if(checkbox.checked){
-            header_div.style.position = "";
-        }
-        else{
-            // write the original style for the header-div out.
-        }
-    });
-});
+function toggleLike(icon) {
+    if (icon.classList.contains('liked')) {
+        icon.classList.remove('liked');
+    } else {
+        icon.classList.add('liked');
+    }
+}
