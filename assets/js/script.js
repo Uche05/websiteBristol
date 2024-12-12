b395fdbc9fdc7569eaf4ab489c24ee7243ba7521
Uche05 <uchekpadeuwa@gmail.com>
2024-12-12 22:06:12 +0000

    add info styling and scripting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// like toggle
 function toggleLike(icon) {
     if (icon.classList.contains('liked')) {
         icon.classList.remove('liked');
@@ -5,3 +6,21 @@ function toggleLike(icon) {
         icon.classList.add('liked');
     }
 }
+
+// pop-up
+const popup = document.getElementById("popup");
+
+function openPopup() {
+    popup.style.display = "flex";
+}
+
+function closePopup() {
+    popup.style.display = "none";
+}
+
+// Close popup
+window.onclick = function (event) {
+    if (event.target === popup) {
+        popup.style.display = "none";
+    }
+};
